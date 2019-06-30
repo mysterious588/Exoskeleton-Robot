4bc78a5dd644ba7303e450f2cb808aa341c10e2d
ahmad <a7mad373737@gmail.com>
2019-06-30 15:43:31 +0200

    Finished PID

--- src/pid_/src/pid.py
+++ src/pid_/src/pid.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python
+
+#TODO plot the results
 
 from simple_pid import PID
 import rospy
-from std_msgs import Float32
-import serial
-from matplotlib import pyplot as plt
+from pid_.msg import motors
 
 # PID constants ##TODO: tune them
 kp = 1.
 kd = 0.1
 ki = 0.2
-
-plt.plot(5, 1)
-plt.show()
 
 # topic to which we will subscribe
 left_leg_topic = "left_leg_angle"
@@ -20,9 +17,8 @@ right_leg_topic = "right_leg_angle"
 left_thigh_topic = ""
 right_thigh_topic = ""
 
-arduino_port = '/dev/ttyACM0'
-
-ser = serial.serial(arduino_port)
+# motors custom message
+mots = motors()
 
 # rate in HZ
 RATE = 50
@@ -37,21 +33,50 @@ pid_right_thigh = PID(kp, ki, kd, setpoint=1, output_limits=(-255, 255), auto_mo
 # stores the array by which we will send our output to motors
 output_list = {}
 
+# robot PWM motors publishers
+pub = rospy.Publisher("motors_PWM", motors, queue_size=10)
+
 
 def left_leg_callback(data):
-    output_list[0] = Float32(pid_right_leg(data.data))
+    output_list[0] = (pid_right_leg(data.data))
+    # extend the motor
+    if output_list[0] >= 0:
+        mots.left_leg_1 = output_list[0]
+        mots.left_leg_2 = 0
+    else
+        # ( the opposite of extend word ) the motor
+        mots.left_leg_1 = 0
+        mots.left_leg_2 = output_list[0]
 
 
 def right_leg_callback(data):
-    output_list[1] = Float32(pid_left_leg(data.data))
+    output_list[1] = (pid_left_leg(data.data))
+    if output_list[1] >= 0:
+        mots.right_leg_1 = output_list[1]
+        mots.right_leg_2 = 0
+    else
+        mots.right_leg_1 = 0
+        mots.right_leg_2 = output_list[1]
 
 
 def left_thigh_callback(data):
-    output_list[2] = Float32(pid_left_thigh(data.data))
+    output_list[2] = (pid_left_thigh(data.data))
+    if output_list[2] >= 0:
+        mots.left_thigh_1 = output_list[2]
+        mots.left_thigh_2 = 0
+    else
+        mots.left_thigh_1 = 0
+        mots.left_thigh_2 = output_list[2]
 
 
 def right_thigh_callback(data):
-    output_list[3] = Float32(pid_right_thigh(data.data))
+    output_list[3] = (pid_right_thigh(data.data))
+    if output_list[3] >= 0:
+        mots.right_thigh_1 = output_list[3]
+        mots.right_thigh_2 = 0
+    else
+        mots.right_thigh_1 = 0
+        mots.right_thigh_2 = output_list[3]
 
 
 def update_tunings(pid, new_kp, new_ki, new_kd):
@@ -79,8 +104,7 @@ def begin_listening():
     # uncomment when the MPU's upper body is finished
     # rospy.Subscriber(left_thigh_topic, Float32, left_thigh_callback)
     # rospy.Subscriber(right_thigh_topic, Float32, right_thigh_callback)
-
-    ser.write(output_list.encode())
+    pub.publish(mots)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
